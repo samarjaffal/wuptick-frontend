0d3cfafcab73000980b1efb0288b5e976dad41fa
samarjaffal <samarjaffalh@gmail.com>
2020-07-18 10:19:36 -0400

    added eslint config files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
 
-ReactDom.render(<h1>Hello world</h1>, document.getElementById("app"));
+ReactDom.render(<h1>Hello world</h1>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  output: {
-    filename: "app.bundle.js",
-  },
+    output: {
+        filename: 'app.bundle.js',
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
     ],
-  },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
 };
