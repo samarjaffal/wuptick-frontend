78751883b3f04fec237a9ac986444ca758fdd156
samarjaffal <samarjaffalh@gmail.com>
2020-12-10 21:22:03 -0400

    added ModuleTabs component

--- src/pages/Module/index.js
+++ src/pages/Module/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import queryString from 'query-string';
+import { useLocation } from '@reach/router';
 import { Helmet } from 'react-helmet';
 import { LoggedLayout } from '../Layouts/LoggedLayout/index';
+import { ModuleTabs } from './ModuleTabs';
 import { Div, FlexCenter } from '../../components/SharedComponents/styles';
 import {
     TopContainer,
@@ -10,7 +13,16 @@ import {
     Filter,
     InputSearch,
 } from './styles';
-export const Module = () => {
+export const Module = ({ id, location }) => {
+    const path = useLocation();
+    const currentURL = path.pathname;
+    const [currentTab, setCurrentTab] = useState(null);
+    const { tab } = queryString.parse(location.search);
+
+    useEffect(() => {
+        setCurrentTab(tab);
+    }, [tab, id]);
+
     return (
         <LoggedLayout>
             <Helmet>
@@ -35,6 +47,12 @@ export const Module = () => {
                             </div>
                         </RightItemsContainer>
                     </TopContainer>
+                    <div className="SideBarContainer">
+                        <ModuleTabs
+                            currentTab={currentTab}
+                            currentURL={currentURL}
+                        />
+                    </div>
                 </div>
             </div>
         </LoggedLayout>
