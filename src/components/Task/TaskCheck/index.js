87a7bc83d681a9ec8c5d7e3c5b7eff23a1452b24
samarjaffal <samarjaffalh@gmail.com>
2020-12-17 14:34:42 -0400

    added task check on module page

---
+++ src/components/Task/TaskCheck/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TaskCheck as TaskCheckStyled } from './styles';
+
+export const TaskCheck = () => {
+    return <TaskCheckStyled />;
+};
+
+TaskCheck.propTypes = {};
---
+++ src/components/Task/TaskCheck/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Colors } from '../../../assets/css/colors';
+
+export const TaskCheck = styled.div`
+    width: 20px;
+    height: 20px;
+    background: ${Colors.softGray};
+    border-radius: 8px;
+`;
