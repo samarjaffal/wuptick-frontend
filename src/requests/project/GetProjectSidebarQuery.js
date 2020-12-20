9c108d4a791be776debaa21716b27a712a037ae4
samarjaffal <samarjaffalh@gmail.com>
2020-12-20 11:22:32 -0400

    fixed bug on MemberModal component on module page

--- src/requests/project/GetProjectSidebarQuery.js
+++ src/requests/project/GetProjectSidebarQuery.js
@@ -1,12 +1,16 @@
 import { useQuery } from 'react-apollo';
 import { gqlGetProjectSidebar } from '../graphql/gqlGetProjectSidebar';
+4;
+import { useUser } from '../../hooks/useUser';
 import PropTypes from 'prop-types';
 
 export const GetProjectSidebarQuery = ({ children, projectId }) => {
+    const { setCurrentProject } = useUser();
     const { error, loading, data } = useQuery(gqlGetProjectSidebar, {
         variables: { projectId },
         onCompleted: (data) => {
             console.log(data, 'data GetProjectSidebarQuery');
+            setCurrentProject(data.getProject);
         },
     });
     if (loading || !data) {
