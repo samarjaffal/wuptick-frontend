c0d7a825bcfc9bea3f692575b8a0ed5c884c27d9
samarjaffal <samarjaffalh@gmail.com>
2021-01-16 08:09:43 -0400

    installed html parser for Editor.js (editorjs-html)

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import EditorJS from '@editorjs/editorjs';
+import edjsHTML from 'editorjs-html';
 import { EDITORCONF } from './editor-conf';
 import { EditorStyle } from '../../assets/css/EditorStyle';
 
 export const Editor = () => {
     const editor = new EditorJS({ ...EDITORCONF });
+    const output = document.getElementById('output');
+
+    const parseToHTMl = (outputData) => {
+        const edjsParser = edjsHTML();
+        let html = edjsParser.parse(outputData);
+        console.log(html, 'html');
+        output.innerHTML = '';
+        html.forEach((element) => {
+            output.innerHTML += element;
+        });
+    };
+    const handleClick = () => {
+        editor
+            .save()
+            .then((outputData) => {
+                console.log('Article data: ', outputData);
+                /* parseToHTMl(outputData); */
+            })
+            .catch((error) => {
+                console.log('Saving failed: ', error);
+            });
+    };
+
     return (
         <>
             <EditorStyle />
             <div id="editor"></div>
-            <button onClick={() => console.log('here')}>Check Data</button>
+            <div id="output"></div>
+            <button onClick={() => handleClick()}>Check Data</button>
         </>
     );
 };
