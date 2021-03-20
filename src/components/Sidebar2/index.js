6aca8982dd5d22ce4b89d6b4b48b9a66123669d1
samarjaffal <samarjaffalh@gmail.com>
2021-03-20 09:45:54 -0400

    remove teamselected from sidebar

--- src/components/Sidebar2/index.js
+++ src/components/Sidebar2/index.js
@@ -42,7 +42,6 @@ export const Sidebar = () => {
                     )}
                 </Me>
                 <Hr />
-                {teamSelected.name}
             </Ul>
         </SidebarStyled>
     );
