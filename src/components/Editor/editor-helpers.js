80984a57b1fc058dab914a8a0830b0b188bd569d
samarjaffal <samarjaffalh@gmail.com>
2021-06-02 22:25:54 -0400

    fix bug on editor files save (editor-helpers)

--- src/components/Editor/editor-helpers.js
+++ src/components/Editor/editor-helpers.js
@@ -1,5 +1,5 @@
 export const urlify = (text) => {
-    var urlRegex = /https?:\/\/(?![^" ]*(?:jpg|png|gif|jpeg|svg|pdf|docx|doc|txt|pptx|ppt|mp3|mp4|xls|html|htm|zip|rar|ai|psd|json|csv))[^" ]+/g;
+    var urlRegex = /https?:\/\/(?![^" ]*(?:jpg|png|gif|jpeg|svg|pdf|docx|doc|txt|pptx|ppt|mp3|mp4|xls|html|htm|zip|rar|ai|psd|json|csv|download))[^" ]+/g;
     return text.replace(urlRegex, function (url) {
         return '<a href="' + url + '" target="_blank">' + url + '</a>';
     });
